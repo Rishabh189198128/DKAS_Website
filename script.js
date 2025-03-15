722c2f254c764736d39acd90e12bf863b6f9a15d
rishabh189198128 <rishabh.189198128@gmail.com>
2025-03-15 19:25:00 +0530

    DKAS

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleNews(id) {
+    // Hide all news details
+    let allNews = document.querySelectorAll(".news-details");
+    allNews.forEach(news => {
+        if (news.id !== `news-${id}`) {
+            news.style.display = "none";
+        }
+    });
+
+    // Toggle the clicked news item
+    let selectedNews = document.getElementById(`news-${id}`);
+    if (selectedNews.style.display === "block") {
+        selectedNews.style.display = "none";
+    } else {
+        selectedNews.style.display = "block";
+    }
+}
+
+
+
